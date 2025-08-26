22e39626cfcc2a141df73dceecf223e593fd2502
kayisshal <kayisshalahuddin.studi@gmail.com>
2025-08-26 15:56:42 +0700

    Update Tugas02.py
    Menambahkan kerangka modular dan menu

--- Tugas02/Tugas02.py
+++ Tugas02/Tugas02.py
@@ -3,11 +3,35 @@ A = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 B = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 for i in range(3):
-  for j in range(3):
-    A[i][j] = int(input())
+    for j in range(3):
+        A[i][j] = int(input())
 
 print("\n")
 
 for i in range(3):
-  for j in range(3):
-    B[i][j] = int(input())
+    for j in range(3):
+        B[i][j] = int(input())
+
+def penambahan_matriks():
+    pass
+
+def pengurangan_matriks():
+    pass
+
+def perkalian_matriks():
+    pass
+
+def cek_matriks_satuan():
+    pass
+
+menu = input()
+if menu == '1' :
+    pass
+elif menu == '2':
+    pass
+elif menu == '3':
+    pass
+elif menu == '4':
+    pass
+else:
+    print("Bukan pilihan yang benar")
